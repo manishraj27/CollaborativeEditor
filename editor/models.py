d515e3ee246c74e8acc5b6107a537bb860d0aa69
manishraj27 <manish012raj@gmal.com>
2024-09-13 14:58:17 +0530

    Users List Implemented

--- editor/models.py
+++ editor/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 
 class Room(models.Model):
-    room_id = models.CharField(max_length=6, unique=True)  # Make sure this is unique
-    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set on creation
+    room_id = models.CharField(max_length=6, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.room_id
-
-
-
 
 class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
@@ -18,3 +15,14 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.room} - {self.sender}"
+
+class RoomUser(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255)
+    connected_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('room', 'username')  # Ensure a user can't be added to the same room multiple times
+
+    def __str__(self):
+        return f"{self.username} in {self.room}"
--- editor/routing.py
+++ editor/routing.py
@@ -1,7 +1,6 @@
-# routing.py
 from django.urls import path
 from editor import consumers
 
 websocket_urlpatterns = [
-    path('ws/messages/<str:room_id>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/messages/<str:room_id>/<str:username>/', consumers.ChatConsumer.as_asgi()),
 ]
