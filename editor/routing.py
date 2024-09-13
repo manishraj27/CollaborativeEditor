f1ee6b1966513edf71c07755322cde83e854b39e
manishraj27 <manish012raj@gmal.com>
2024-09-13 17:55:28 +0530

    Styling to index.html

--- editor/routing.py
+++ editor/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from editor import consumers
 
 websocket_urlpatterns = [
-    path('ws/messages/<str:room_id>/<str:username>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/messages/<str:room_id>/<str:username>/', consumers.ChatConsumer.as_asgi()), #you have to do wss while deployment since it will be https
 ]
